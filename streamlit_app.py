4d5b332ee5dacb0336016bf83e3b530a9b7139e2
Alvia01 <152358126+Alvia01@users.noreply.github.com>
2023-11-29 12:31:44 +0800

    Update streamlit_app.py
    edited menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu:
-Avocado and Egg
-Spinach and fruitshake')
+streamlit.title('Breakfast Menu:')
+streamlit. title('Avocado and Egg')
+streamlit.title ('Spinach and fruitshake')
